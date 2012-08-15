71c52f61fce7ed55ac6f82672d3805979ea018ef
Ryan Neufeld <ryan@neucode.org>
2012-08-15 08:06:46 -0700

    Renaming REST Controller to be a custom core class. This removes the need for an include statements in child classes

--- application/core/CI_RESTController.php
+++ application/core/CI_RESTController.php
@@ -13,7 +13,7 @@
  * @link			https://github.com/philsturgeon/codeigniter-restserver
  * @version 		2.6.0
  */
-abstract class REST_Controller extends CI_Controller
+abstract class CI_RESTController extends CI_Controller
 {
 	/**
 	 * This defines the rest format.
